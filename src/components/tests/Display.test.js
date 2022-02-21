c222b7b809bfc28df85015d856cfed431b18af30
Chris Kelley <chriskelley85@gmail.com>
2022-02-21 14:17:59 -0600

    added tests to display and episode

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -2,10 +2,44 @@ import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Display from './../Display';
+jest.mock('./../../api/fetchShow');
+import Display from './../Display';
 
+const data = {
+    name: "",
+    summary: "",
+    seasons: [
+        { id: 1, name: "caterpillar", episodes: [] },
+        { id: 2, name: "cocoon", episodes: [] },
+        { id: 3, name: "butterfly", episodes: [] }
+    ]
+}
 
-test('renders without errors with no props', async () => { });
+test('renders without errors with no props', ()=>{
+    render(<Display />);
+});
 
-test('renders Show component when the button is clicked ', () => { });
+test('renders Show component when the button is clicked ', ()=>{
+    fetchShow.mockResolvedValueOnce(data);
 
-test('renders show season options matching your data when the button is clicked', () => { });
+    render(<Display show={data} />);
+
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    const showComponent = await screen.findByTestId("show-container");
+
+    expect(showComponent).toBeInTheDocument();
+});
+
+test('renders show season options matching your data when the button is clicked', ()=>{
+    fetchShow.mockResolvedValueOnce(data);
+
+    render(<Display />);
+
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    
+    await waitFor(() => {
+        const seasons = screen.getAllByTestId("season-option");
+        expect(seasons).toHaveLength(3);
+    });
--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -2,9 +2,29 @@ import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
+const data = {
+    id: 909347,
+    image: "https://static.tvmaze.com/uploads/images/medium_landscape/132/332055.jpg",
+    name: "Chapter Seven: The Lost Sister",
+    season: 2,
+    number: 7,
+    summary: "El travels to Chicago to find her sister Eight, aka Kali, who has teamed up with other outcasts to kill the men who tormented her at Hawkins Labs.",
+    runtime: 46
+}
 
-test("renders without error", () => { });
+test("renders without error", () => {
+    render(<Episode episode={data} />);
+});
 
-test("renders the summary test passed as prop", () => { });
+test("renders the summary test passed as prop", ()=>{
+    render(<Episode episode={data}/>);
 
-test("renders default image when image is not defined", () => { });
+const sum = screen.queryByText(/El travels to Chicago to find her sister Eight, aka Kali, who has teamed up with other outcasts to kill the men who tormented her at Hawkins Labs/i);
+
+expect(sum).toBeTruthy();
+});
+
+test("renders default image when image is not defined", ()=>{
+    render(<Episode episode={data} />);
+
+});
